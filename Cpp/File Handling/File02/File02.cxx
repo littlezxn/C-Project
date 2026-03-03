ba1bef6e45c2cf2749ed02486e475977fd1e2d78
C++ project <zaynproton001@proton.me>
2026-03-03 22:05:30 +0530

    FIle handling 02

--- Cpp/File Handling/File02/File02.cxx
+++ Cpp/File Handling/File02/File02.cxx
@@ -1,16 +1,18 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include<iostream> //header file for input and output
+#include<fstream> //header file for file handling
+#include<string> //header file for string
 using namespace std;
 int main(){
-    ofstream fout("sample.txt");
-    fout<<"Enter whatever you want.";
-    fout.close();
+    ofstream fout("sample.txt"); //ofstream for write
+    fout<<"Enter whatever you want: ";
+    fout.close(); //closing the file 
 
     string text;
-    ifstream fin("sample.txt");
+    ifstream fin("sample.txt"); //ifstream for read 
     getline(fin , text);
     cout<<text<<endl;
-    fin.close();
+    fin.close(); //closing the file
     return 0;
 }
+//end of the program 
+//you can see the outputr in sample.txt
