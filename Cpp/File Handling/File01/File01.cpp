fe68804c81c50354dc6f0000f94b1dc8d6fec8de
C++ project <zaynproton001@proton.me>
2026-03-03 20:27:46 +0530

    first project

--- Cpp/File Handling/File01/File01.cpp
+++ Cpp/File Handling/File01/File01.cpp
@@ -1,9 +1,12 @@
-#include<iostream>
-#include<fstream>
+#include<iostream> //header file for input and output
+#include<fstream> //header file for file handling
 using namespace std;
-int main(){
-    ofstream fout("sample.txt");
+// main function
+int main(){ 
+    ofstream fout("sample.txt"); //ofstream it will gonna create file and print the input as well
     fout<<"Hello World";
-    fout.close();
+    fout.close(); //closing the file 
     return 0;
 }
+//end of the program 
+//you can see the outputr in sample.txt
